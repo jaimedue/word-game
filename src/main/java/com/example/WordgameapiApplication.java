a3bb7100707bf6e074d2b58ccd2b093e13e7f844
jaimedue <jdue930@gmail.com>
2022-05-31 12:07:39 -0500

    set up for heroku deployment

--- src/main/java/com/example/WordgameapiApplication.java
+++ src/main/java/com/example/WordgameapiApplication.java
@@ -2,8 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class WordgameapiApplication {
 
 	public static void main(String[] args) {

bdb920f3c1e91ae424b203994c362e25b0c0bd48
jaimedue <jdue930@gmail.com>
2022-06-07 17:16:07 -0500

    cors

--- src/main/java/com/example/controller/WordController.java
+++ src/main/java/com/example/controller/WordController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import com.example.model.Word;
 import com.example.service.WordServiceInterface;
 
 @RestController()
+@CrossOrigin(origins = "https://misspelledwordgame.herokuapp.com")
 @RequestMapping("/api/words")
 public class WordController {
 	
